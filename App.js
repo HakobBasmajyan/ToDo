baa694259ae3a0454472c7c251eea658a529429e
HakobBasmajyan <105809295+HakobBasmajyan@users.noreply.github.com>
2023-03-19 14:58:09 +0400

    second

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import FormInput from './components/FormInput'
+import List from './components/List'
+import { Footer} from './components/Footer'
+import { DataProvider } from './components/DataProvider'
+
+
+export default function App() {
+  return (
+    <DataProvider>
+    <div className='App'>
+        <h1>To Do List</h1>
+        <FormInput />
+          <List />
+        <Footer />
+      </div>
+    </DataProvider>
+  )
+}
+
